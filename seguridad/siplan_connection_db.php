c1d43d08d54f81a136ca9f78304811dc9adbeda8
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-06 12:28:09 -0600

    Se agregan comentarios

--- seguridad/siplan_connection_db.php
+++ seguridad/siplan_connection_db.php
@@ -1,5 +1,6 @@
 <?php
 class conectar{
+    //Se crean las variables a utilizar en la clase de la conexión
 	protected $servidor;
 	protected $usuario;
 	protected $clave;
@@ -7,6 +8,7 @@ class conectar{
 	protected $conexion;
     protected $error = "Error en la Conexión a la Base de Datos, favor de contactar al webmaster";
 
+    //Se inicializan las variables para la conexión a la base de datos
     function inicializar($h,$u,$p,$b){
      $this->servidor = $h;
      $this->usuario = $u;
@@ -14,6 +16,7 @@ class conectar{
      $this->bd = $b;
     }
 
+    //Se realiza la conexión y se regresa el recurso ocn la conexión o con el error
 	function conectarse(){
      $this->conexion = new mysqli($this->servidor,$this->usuario,$this->clave,$this->bd);
     if ($this->conexion->connect_errno) {
