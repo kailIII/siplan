3a40c72657654366457d7c49438f3fc011fc1202
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-06 12:53:02 -0600

    se eliminan la interaccion con iexplorer de windows, no requerida

--- administrador/header.php
+++ administrador/header.php
@@ -15,12 +15,7 @@ if(isset($_POST['id_dep'])){
 }
 ?>
 <!DOCTYPE html>
-<!--[if IE 8]>
-<html xmlns="http://www.w3.org/1999/xhtml" class="ie8 wp-toolbar"  lang="es-ES">
-<![endif]-->
-<!--[if !(IE 8) ]><!-->
 <html xmlns="http://www.w3.org/1999/xhtml" class="wp-toolbar"  lang="es-ES">
-<!--<![endif]-->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>.:: SIPLAN ::..</title>
