c9b3782eef2fbd7b38a5f29d897a40bb79e8da0b
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-09 11:49:17 -0600

    se elimina la dobe inicio de sesion, bug elimnado

--- seguridad/reg_vars.php
+++ seguridad/reg_vars.php
@@ -1,8 +1,8 @@
 <?php
-session_start(); //agregado 17-10-2014
-session_cache_limiter('nocache,private'); //agregado 17-10-2014
-/*------- definicion de variables de sesion y su contenido  ----------------*/
 
+$cdu="SELECT * FROM usuarios WHERE usuario = '$usuario' AND password = '$cve'";
+$res_usr = $conexion->query($cdu);
+$info=$res_usr->fetch_assoc();
 $_SESSION['sesion_activa_v3'] = 1;
 $_SESSION['id_usuario_v3'] = $info['id_usuario'];
 $_SESSION['id_dependencia_v3'] = $info['id_dependencia'];
@@ -14,14 +14,13 @@ $_SESSION['trim_habilitado_v3'] = $info['trim_habilitado'];
 $_SESSION['inicio_poa01'] = $info['i_captura_prog01'];
 $_SESSION['fin_poa01']= $info['f_captura_prog01'];
 $_SESSION["autentificado_v3"] = "SI";
-$_SESSION["ejercicio_v3"] = "2014";//date('Y');
-
-$consulta_nombre_dependencia = "SELECT * FROM dependencias WHERE id_dependencia = ".$_SESSION['id_dependencia_v3'] = $info['id_dependencia'];
-$cons_dep = @mysql_query ($consulta_nombre_dependencia,$siplan_data_conn);
-$data_dep = mysql_fetch_array($cons_dep);
-
+$_SESSION["ejercicio_v3"] = $ejercicio;
+$res_usr->free();
+$consulta_nombre_dependencia = "SELECT * FROM dependencias WHERE id_dependencia = ".$_SESSION['id_dependencia_v3'];
+$cons_dep = $conexion->query($consulta_nombre_dependencia);
+$data_dep = $cons_dep->fetch_assoc();
 $_SESSION['nombre_dependencia_v3'] =  $data_dep['nombre'];
 $_SESSION['sector_dependencia_v3'] =  $data_dep['id_sector'];
-
+$cons_dep->free();
 
 ?>
