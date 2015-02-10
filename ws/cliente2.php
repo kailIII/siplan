811573ab035d5a65f8b80850b47ad58be80f44d3
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-10 11:39:29 -0600

    cambio de direccion de webservice a local

--- ws/cliente2.php
+++ ws/cliente2.php
@@ -1,21 +1,13 @@
-<?php
-ini_set('display_errors','1');
-
-
-?>
+<?php ini_set('display_errors','1'); ?>
 <html>
-    <head><title>cliente webservice
-            !!</title>
-    <meta charset="utf-8"></head>
-
-    <body>
-
+<head>
+<title>cliente webservice!!</title>
+<meta charset="utf-8"></head>
+<body>
 <?php
-
-
 require_once('lib/nusoap.php');
-//$client = new nusoap_client("http://localhost/siplan/ws/ws_siplan.php?wsdl", true);
-$client = new nusoap_client("http://siplan.zacatecas.gob.mx/ws/ws_siplan.php?wsdl", true);
+$client = new nusoap_client("http://localhost/siplan/ws/ws_siplan.php?wsdl", true);
+//$client = new nusoap_client("http://siplan.zacatecas.gob.mx/ws/ws_siplan.php?wsdl", true);
 $client->setCredentials("siplan_ws","4Do1t4m0");
 $err = $client->getError();
 if ($err) {
@@ -28,9 +20,9 @@ $result = $client->call('wsC73',array('ejercicio' => 2015,'dependencia'=>1));
 //$result = $client->call('wsC76',array('ejercicio' => 2014,'dependencia'=>62));
 //$result = $client->call('wsC77',array('ejercicio' => 2014,'dependencia'=>62));
 //$result = $client->call('wsR',array('oficio' => 'UPLA-OE/0063/14','respuesta'=>1));
-
 echo "<pre>";
 print_r($result);
 echo "</pre>";
 ?>
- </body></html>
+</body>
+</html>
