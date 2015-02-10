acc48e96ca649b4d7159701a4d924fb16e097c96
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-10 11:12:34 -0600

    archivo de seguridad del ws

---
+++ ws/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>.::SIPLAN 2013::.</title>
+<script type="text/javascript">
+    var pagina = 'http://siplan2013.zacatecas.gob.mx';
+    var segundos = 3;
+    function redireccion() {
+
+        document.location.href=pagina;
+
+    }
+    setTimeout("redireccion()",segundos);
+</script>
+</head>
+<body onLoad="document.location.href='http://siplan2013.zacatecas.gob.mx'">
+</body>
+</html>
