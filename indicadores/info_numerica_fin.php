edd347360961a84772b96a5554a270c254fa5608
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-11 16:30:26 -0600

    nuevo

---
+++ indicadores/info_numerica_fin.php
@@ -0,0 +1,10 @@
+ <ul class="nav nav-tabs">
+  <li role="presentation" id="basica" ><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=94&info=1&tipoind=1">Información Básica</a></li>
+  <li role="presentation" id="numerica" class="active"><a href="#">Información Númerica</a></li>
+  <li role="presentation" id="grafica"><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=94&info=3&tipoind=1">Gráficos</a></li>
+</ul>
+
+
+        <form>
+
+        </form>
