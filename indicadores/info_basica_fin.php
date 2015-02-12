f7bb74b876fef050bd3d1d63d0e88e7ddf3df6f6
martinsillo <martinsillo.gamboa@gmail.com>
2015-02-12 00:08:00 -0600

    se agregan textbox y se eliminan campos de la consulta

--- indicadores/info_basica_fin.php
+++ indicadores/info_basica_fin.php
@@ -10,8 +10,6 @@ pr.nombre as proyecto,
 indpro.fin_objetivo as objetivo,
 indpro.nombre_fin as nombre,
 indpro.metodo_fin as metodo,
-indpro.fin_calculado as calculado,
-indpro.fin_resultado as resultado,
 indpro.fin_observaciones as observaciones
 FROM proyectos pr
 inner join dependencias dep on(pr.id_dependencia = dep.id_dependencia)
@@ -27,10 +25,21 @@ $res_consulta = $ex_consulta->fetch_assoc();
   <li role="presentation" id="numerica"><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=94&info=2&tipoind=1">Información Númerica</a></li>
   <li role="presentation" id="grafica"><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=94&info=3&tipoind=1">Gráficos</a></li>
 </ul>
-
-
         <form>
-            <pre>
-            <?php print_r($res_consulta); ?>
-            </pre>
+            <div class="input-group">
+  <span class="input-group-addon" id="basic-addon1"><b>Sector</b></span>
+  <input type="text" class="form-control" aria-describedby="basic-addon1" value="<?php echo $res_consulta['num_sector']." - ".$res_consulta['sector'];?>">
+</div>
+<br>
+            <div class="input-group">
+  <span class="input-group-addon" id="basic-addon1"><b>Dependencia</b></span>
+  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+</div>
+<br>
+            <div class="input-group">
+  <span class="input-group-addon" id="basic-addon1"><b>Proyecto</b></span>
+  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+</div>
+
+
         </form>
