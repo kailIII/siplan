e87a6e98798c2028d90bb1ea4cfd66ff451220ec
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2015-02-13 13:55:51 -0600

    cambiuo_menu

--- indicadores/info_numerica_fin.php
+++ indicadores/info_numerica_fin.php
@@ -16,9 +16,9 @@ $ex_consulta = $mysqli->query($consulta);
 $res_consulta = $ex_consulta->fetch_assoc();
 ?>
 <ul class="nav nav-tabs">
-  <li role="presentation" id="basica" ><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=94&info=1&tipoind=1">Información Básica</a></li>
+  <li role="presentation" id="basica" ><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=<?php echo $_GET['idproyecto'];?>&info=1&tipoind=1">Información Básica</a></li>
   <li role="presentation" id="numerica" class="active"><a href="#">Información Númerica</a></li>
-  <li role="presentation" id="grafica"><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=94&info=3&tipoind=1">Gráficos</a></li>
+  <li role="presentation" id="grafica"><a href="main.php?token=17e62166fc8586dfa4d1bc0e1742c08b&idproyecto=<?php echo $_GET['idproyecto'];?>&info=3&tipoind=1">Gráficos</a></li>
 </ul>
 <p><span class="label label-default" style="font-size:12px;">Proyecto: </span>&nbsp;<strong><?php echo $res_consulta['num_proyecto']." ".$res_consulta['proyecto']; ?></strong> </p>
 <p><span class="label label-default" style="font-size:12px;">Nombre del indicador: </span>&nbsp;<strong><?php echo $res_consulta['nombre']; ?></strong> </p>
