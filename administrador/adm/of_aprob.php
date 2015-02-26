6026aa791499780c6b3952c67b62b9821e31c02b
martinsillo <martinsillo.gamboa@gmail.com>
2015-02-26 14:41:55 -0600

    modified

--- administrador/adm/of_aprob.php
+++ administrador/adm/of_aprob.php
@@ -1,11 +1,14 @@
-<?php date_default_timezone_set('UTC'); ?>
+
+<?php
+date_default_timezone_set('UTC');
+?>
 <script src="SpryAssets/SpryTabbedPanels.js" type="text/javascript"></script>
 <link href="SpryAssets/SpryTabbedPanels.css" rel="stylesheet" type="text/css" />
 <div class="wrap">
 <h2><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Oficios de Aprobación</h2>
 <br>
 <div class="panel panel-default">
-<div class="panel-heading"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Oficios de Aprobación enviados a Finanzas</div>
+<div class="panel-heading"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Oficios de Aprobación enviados a Finanzas - </div>
 <div class="panel-body">
 <div id="TabbedPanels1" class="TabbedPanels">
   <ul class="TabbedPanelsTabGroup">
